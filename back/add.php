b479dc556a44bfcc02aceefad74dfa74a48d6fd0
minder309 <minder309@gmil.com>
2022-11-30 15:51:02 +0800

    modified:   back/add.php add.php完成。下拉是選單完成。

--- back/add.php
+++ back/add.php
@@ -41,9 +41,58 @@
             <label class="col-form-label col-md-4" style="text-align-last:justify">科系</label>
             <select class="form-control col-md-8" name="dept">
                 <?php
-                    // 從`dept`
-                
-                
-                
-                
+                    // 從`dept`資料表終撈出所有科系資料並在網頁上建立下拉選單項目。
+                    $sql="SELECT * FROM `dept`";
+                    $depts=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                    // fetchAll(PDO::FETCH_ASSOC)為回傳有資料，並組成陣列。
+                    foreach($depts as $dept){
+                        echo "<option value='{$dept['id']}'>{$dept['name']}</option>";
+                        // 做成下拉是選單，並且vaule為id，內部顯示為$dept['name']。
+                    }
                 ?>
+            </select>
+        </div>
+        <div class="form-group row">
+        <label class="col-form-label col-md-4" style="text-align-last:justify">畢業國中</label>
+        <select class="form-control col-md-8" name="graduate_at" >
+            <?php 
+            //從`graduate_school`t資料表中撈出所有的畢業學生資料並在網頁上製作成下拉選單的項目
+            $sql="SELECT * FROM `graduate_school`";
+            $grads=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+            foreach($grads as $grad){
+                echo "<option value='{$grad['id']}'>{$grad['county']}{$grad['name']}</option>";
+            }
+            ?>
+        </select>
+    </div>
+    <div class="form-group row">
+        <label class="col-form-label col-md-4" style="text-align-last:justify">畢業狀態</label>
+        <select class="form-control col-md-8" name="status_code" >
+            <?php 
+            //從`status`資料表中撈出所有的畢業狀態並在網頁上製作成下拉選單的項目
+            $sql="SELECT * FROM `status`";
+            $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+            foreach($rows as $row){
+                echo "<option value='{$row['code']}'>{$row['status']}</option>";
+            }
+            ?>
+        </select>
+    </div>
+    <div class="form-group row">
+        <label class="col-form-label col-md-4" style="text-align-last:justify">班級</label>
+        <select class="form-control col-md-8" name="class_code">
+            <?php
+            //從`classes`資料表中撈出所有的班級資料並在網頁上製作成下拉選單的項目
+            $sql="SELECT `id`,`code`,`name` FROM `classes`";
+            $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+            foreach($rows as $row){
+                echo "<option value='{$row['code']}'>{$row['name']}</option>";
+            }
+            ?>
+        </select>                
+    </div>
+   <div class="text-center">
+       <input class="btn btn-primary" type="submit" value="確認新增">
+   </div>
+</form>
+</div>
